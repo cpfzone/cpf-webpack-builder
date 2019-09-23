3afec3b5b7a154505219137acc766a96e891d8d4
cpfzone <cpfzone@msn.com>
2019-09-23 09:19:13 +0800

    上传代码

---
+++ test/unit/webpack-base-test.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const assert = require('assert');
+
+
+describe("webpack.base.js test case", ()=>{
+    const baseConfig = require('../../lib/webpack.base');
+    console.log(baseConfig);
+    it('entry', ()=>{
+        assert.equal(baseConfig.entry.index,"C:/Users/cpfzo/Desktop/demo/webpack-item1/builder-webpack/test/smoke/template/src/index/index.js");
+        assert.equal(baseConfig.entry.search,"C:/Users/cpfzo/Desktop/demo/webpack-item1/builder-webpack/test/smoke/template/src/search/index.js");
+    })
+})
