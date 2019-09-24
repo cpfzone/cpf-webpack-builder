ca29c2c459565f54d8c5a4d3fd0bdcfd8a30cf10
cpfzone <cpfzone@msn.com>
2019-09-24 08:15:55 +0800

    修改代码

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 describe("webpack.base.js test case", ()=>{
     const baseConfig = require('../../lib/webpack.base');
-    console.log(baseConfig);
+    console.log(baseConfig,"config ");
     it('entry', ()=>{
         assert.equal(baseConfig.entry.index,"C:/Users/cpfzo/Desktop/demo/webpack-item1/builder-webpack/test/smoke/template/src/index/index.js");
         assert.equal(baseConfig.entry.search,"C:/Users/cpfzo/Desktop/demo/webpack-item1/builder-webpack/test/smoke/template/src/search/index.js");
